59721c5d8c0766e1f1373d196cc31dab7c5cb1d2
Ryan Jones <RyanJonesHTX@pm.me>
2022-10-03 00:44:36 -0500

    added .env & added db info

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 const express = require('express')
 //creates a variable called 'app' that we can use throughout the file
 const app = express()
+//Variable for PORT
+const PORT = 3000
 //Initialize variable to hold require function for MongoDB
 const MongoClient = require('mongodb').MongoClient
+//Initialize dotenv
+require('dotenv').config()
 
 
 //creates a server that browsers can connect to that will serve necessary files & info
-app.listen(3000, ()=>{
-    console.log('listening on 3000')
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`listening on ${PORT}`)
 })
 
 
@@ -22,20 +26,24 @@ app.get('/', (req, res)=>{
 })
 
 //Connect to MongoDB
-//get MongoDB connection string from Ryan
-MongoClient.connect('mongodb-connection-string', 
+let dbConnectionStr = process.env.DB_STRING
+    dbName = 'motivational'
+    dbCollection = 'quotes'
+//get MongoDB connection string from Ryan ^^
+MongoClient.connect(dbConnectionStr, 
 { useUnifiedTopology: true })
     .then(client => {
-        console.log('Connected to Database')
+        console.log(`Connected to the ${dbName} Database`)
         //initialize database name
         //get database name from Ryan
-        const db = client.db('database-name')
+        const db = client.db(dbName)
         //initialize name of collection
         //get collection name from Ryan - quotes?
-        const quotesCollection = db.collection('collection-name')
+        const quotesCollection = db.collection(dbCollection)
 
         // ** Ryan - set view engine to ejs (must be addeed before use, get, or post methods)
-
+        
+        // app.set('view engine', 'ejs')
 
         app.use()
 
